d447ac0bd6077843db8e4468dfcd9e79acf29b47
Mohid Kazi <mohidkazix@gmail.com>
2024-11-26 16:41:27 +0530

    fix(meta): update meta information

---
+++ components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sticky top-0 z-50 toolbar bg-gray-100 bg-opacity-10 backdrop-blur py-3 pl-12  flex items-center px-4 shadow-sm">
+    <!-- Window Control Buttons -->
+    <div class="flex space-x-2">
+      <!-- Close Button -->
+      <button
+        class="h-3.5 w-3.5 bg-red-500 rounded-full border border-gray-300 hover:bg-red-600 transition"
+        aria-label="Close"
+      ></button>
+      <!-- Minimize Button -->
+      <button
+        class="h-3.5 w-3.5 bg-yellow-500 rounded-full border border-gray-300 hover:bg-yellow-600 transition"
+        aria-label="Minimize"
+      ></button>
+      <!-- Fullscreen Button -->
+      <button
+        class="h-3.5 w-3.5 bg-green-500 rounded-full border border-gray-300 hover:bg-green-600 transition"
+        aria-label="Fullscreen"
+      ></button>
+    </div>
+
+    <!-- Toolbar Title (Optional) -->
+    <div class="flex-grow text-center flex justify-center text-gray-600 dark:text-gray-300 text-sm">
+      <img src="/public/my-logo.png" height="35" width="35" alt="m" class="">
+    </div>
+  </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="min-h-screen relative">
     <Navbar />
+    <!-- <ToolBar /> -->
     <Hero />
     <Social />
     <section class="relative mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 overflow-hidden">
@@ -11,8 +12,11 @@
 
 <script setup>
 const metaConfig = {
-  title: "Mohid Kazi - Experience",
-  description: "Mohid kazi portfolio website to showcase experience and resume",
+  author: "Mohid Kazi",
+  title: "Experience | Mohid Kazi ",
+  description: "A detailed Experience portfolio website for Mohid Kazi",
+  keywords: "Mohid Kazi, portfolio, experience, freelancing, projects, readme, experience website, freelancer, freelancing",
+  favicon: "https://experience.mohidkazi.com/favicon.ico",
   image: "https://experience.mohidkazi.com/m.png",
   url: "https://experience.mohidkazi.com",
 }
@@ -24,10 +28,16 @@ useHead({
       name: "description",
       content: metaConfig.description,
       meta: [
+        { name: "author", content: metaConfig.author },
+        { name: "keywords", content: metaConfig.keywords },
         { name: "og:title", content: metaConfig.title },
         { name: "og:description", content: metaConfig.description },
         { name: "og:image", content: metaConfig.image },
         { name: "og:url", content: metaConfig.url },
+        { name: "apple-touch-icon", content: metaConfig.image },
+        { name: "shortcut icon", content: metaConfig.favicon },
+        { name: "icon", content: metaConfig.image },
+        { name: "og:type", content: "website" },
         { name: "twitter:title", content: metaConfig.title },
         { name: "twitter:description", content: metaConfig.description },
         { name: "twitter:image", content: metaConfig.image },
